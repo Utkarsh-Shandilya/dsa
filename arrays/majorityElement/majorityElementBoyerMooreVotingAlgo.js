7cfbcbcfdca1b81a9d71c1d65307a64c93f65e25
Utkarsh-Shandilya <utkarshshandilya248@gmail.com>
2025-03-27 22:05:19 +0530

    added sol for majority element with boyer moore voting algorithm

---
+++ arrays/majorityElement/majorityElementBoyerMooreVotingAlgo.js
@@ -0,0 +1,13 @@
+const majorityElement = function (nums) {
+    let candidate = nums[0];
+    let count = 1;
+    for (let i = 1; i < nums.length; i++) {
+        if (candidate === nums[i]) count++;
+        else count--;
+        if (count === 0) {
+            candidate = nums[i];
+            count = 1;
+        }
+    }
+    return candidate;
+};
